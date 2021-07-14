c087954220e99239037290c7c6e37f7fdb15fea5
evopen <520dhh@gmail.com>
2021-07-14 17:50:16 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,6 @@ extern crate rocket;
 
 use binance::account::Account;
 use binance::api::Binance;
-use binance::market::Market;
 use rocket::routes;
 
 async fn get_string(req: String) -> String {
@@ -25,7 +24,7 @@ async fn get_sina_current_price(raw: String) -> Option<String> {
     }
 }
 
-#[get("/mytrades/binance/me/<ticker>")]
+#[get("/mytrades/binance/<ticker>")]
 async fn binance_trades(ticker: String) -> String {
     let result = std::thread::spawn(|| {
         let api_key = Some(std::env::var("BINANCE_API_KEY").unwrap());
@@ -33,6 +32,56 @@ async fn binance_trades(ticker: String) -> String {
         let account: Account = Binance::new(api_key, secret_key);
         let history = account.trade_history(ticker).unwrap();
         history
+    })
+    .join()
+    .unwrap();
+
+    serde_json::to_string(&result).unwrap()
+}
+
+#[get("/mytrades/binance")]
+async fn binance_trades_all() -> String {
+    #[derive(serde::Serialize)]
+    struct Trade {
+        symbol: String,
+        price: f64,
+        qty: f64,
+        date: String,
+        timestamp: u64,
+        transaction_cost: String,
+        transaction_asset: String,
+    }
+    let symbols = vec![
+        "BNBUSDT", "ETHUSDT", "DOGEUSDT", "TRXUSDT", "UNIUSDT", "BTCUSDT",
+    ];
+    let result = std::thread::spawn(|| {
+        let api_key = Some(std::env::var("BINANCE_API_KEY").unwrap());
+        let secret_key = Some(std::env::var("BINANCE_SECRET_KEY").unwrap());
+        let account: Account = Binance::new(api_key, secret_key);
+        let mut trades = symbols
+            .into_iter()
+            .map(|symbol| {
+                account
+                    .trade_history(symbol)
+                    .unwrap()
+                    .into_iter()
+                    .map(|t| Trade {
+                        symbol: symbol.into(),
+                        price: t.price,
+                        qty: t.qty,
+                        date: chrono::NaiveDateTime::from_timestamp((t.time / 1000) as i64, 0)
+                            .format("%m/%d/%Y")
+                            .to_string(),
+                        timestamp: t.time,
+                        transaction_cost: t.commission,
+                        transaction_asset: t.commission_asset,
+                    })
+                    .collect::<Vec<Trade>>()
+            })
+            .flatten()
+            .collect::<Vec<_>>();
+        trades.sort_by_key(|t| t.timestamp);
+        trades
     })
     .join()
     .unwrap();
@@ -74,5 +123,8 @@ async fn index() -> &'static str {
 #[launch]
 fn rocket() -> _ {
     dotenv::dotenv().ok();
-    rocket::build().mount("/", routes![index, get_ticker_price, binance_trades])
+    rocket::build().mount(
+        "/",
+        routes![index, get_ticker_price, binance_trades, binance_trades_all],
+    )
 }
